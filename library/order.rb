f00bba442e5dee84b2cc0f62100813b269449ec8
mclegea <mclegea@gmail.com>
2015-05-13 23:49:33 +0300

    Update order.rb

--- library/order.rb
+++ library/order.rb
@@ -1,1 +1,18 @@
-
+module Library
+  
+  class Order
+    attr_accessor :book, :reader, :date
+  
+    def initialize(book, reader)
+      self.book   = book if is_a? Book
+      self.reader = reader if is_a? Reader
+      self.date   = Time.new
+    end
+    
+    def to_s
+      puts "Book \"#{title}\", reader: #{reader}, order's date: #{date.strftime "%d-%m-%Y"}"
+    end
+  
+  end
+  
+end
