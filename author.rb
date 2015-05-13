f3f2f4611da62ad50991557353e4b4ac28683cc3
mclegea <mclegea@gmail.com>
2015-05-13 03:32:47 +0300

    Update author.rb

--- author.rb
+++ author.rb
@@ -1,1 +1,15 @@
+module Library
+  class Author
+    attr_accessor :name, :biography
 
+    def initialize(name, biography=nil)
+      self.name = name
+      self.biography = biography
+    end
+
+   
+    def to_s
+      "#{name}"
+    end
+  end
+end
