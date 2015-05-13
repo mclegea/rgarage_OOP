bcf9f84807a9a6cef530aaefb2e6a56ef3144868
mclegea <mclegea@gmail.com>
2015-05-13 23:48:52 +0300

    Update book.rb

--- library/book.rb
+++ library/book.rb
@@ -1,1 +1,18 @@
-
+module Library
+  
+  class Book
+    
+    attr_accessor :title, :author
+  
+    def initialize(title, author)
+      self.title  = title
+      self.author = author
+    end
+    
+    def to_s
+      puts "Book \"#{title}\" by #{author}"
+    end
+  
+  end
+  
+end
